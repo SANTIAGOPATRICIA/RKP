98536991430ad9d6d13a85b8c6f8181e753548b1
Clarice Aoto <83498839+Clarice-Satiko-Aoto@users.noreply.github.com>
2024-08-08 15:51:39 -0300

    Add files via upload

--- utils/funcoes.py
+++ utils/funcoes.py
@@ -16,18 +16,45 @@ import sqlite3
 # Cor preta
 black_color = RGBColor(0, 0, 0)
 
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect('database.db')  # Substitua 'database.db' pelo caminho correto
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS propostas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_cliente TEXT,
+            objeto_contencioso TEXT,
+            instancia_superior TEXT,
+            orgao TEXT,
+            itens_atuacao TEXT,
+            pro_labore_inicial TEXT,
+            parcelamento TEXT,
+            numero_parcelas_formatado TEXT,
+            valor_entrada TEXT,
+            parcelamento_restante TEXT,
+            pro_labore_manutencao TEXT,
+            pro_labore_manutencao_valor_sm TEXT,
+            tipo_exito TEXT,
+            exito_percentual_formatado TEXT,
+            exito_texto TEXT,
+            exito_valor_teto TEXT,
+            tempo_expectativa TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
 
 def load_data():
-    try:
-        conn = sqlite3.connect('database.db')  # Substitua 'database.db' pelo caminho correto
-        c = conn.cursor()
-        c.execute("SELECT * FROM propostas")
-        data = c.fetchall()
-        conn.close()
-        return data
-    except sqlite3.Error as e:
-        st.error(f"Erro ao carregar dados do banco de dados: {e}")
-        return None
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM propostas")
+    data = c.fetchall()
+    conn.close()
+    return data
 
 def atualizar_base_dados():
     """
